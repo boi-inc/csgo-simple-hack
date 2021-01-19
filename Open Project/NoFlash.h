1bf7ba5c4535e1d8b12cb65a838d1c347b64ee4e
Bartek <63854737+dek-o-yyy@users.noreply.github.com>
2021-01-19 07:56:06 +0100

    Update NoFlash.h

--- Open Project/NoFlash.h
+++ Open Project/NoFlash.h
@@ -4,17 +4,17 @@
 #include "Features.h"
 #include "Important.h"
 
+
 void noFlash() {
 	while (true)
 	{
 		Sleep(1);
 		if (toggleNoFlash)
 		{
-			DWORD LocalPlayer = readMem<DWORD>(clientBase + dwLocalPlayer);
-			int Flash = readMem<int>(LocalPlayer + m_flFlashDuration);
+			int Flash = readMem<int>(Entity.LocalPlayer + Offsets.m_flFlashDuration);
 			if (Flash > 0)
 			{
-				writeMem<int>(LocalPlayer + m_flFlashDuration, 0);
+				writeMem<int>(Entity.LocalPlayer + Offsets.m_flFlashDuration, 0);
 			}
 		}
 		else if (GetAsyncKeyState(VK_F3) && toggleNoFlash)
